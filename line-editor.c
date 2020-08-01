0e2a050c4fe611b204543082aa6a77ee03ad183e
Přemysl Eric Janouch <p@janouch.name>
2020-08-01 14:04:10 +0200

    Name change

--- line-editor.c
+++ line-editor.c
@@ -1,7 +1,7 @@
 /*
  * line-editor.c: a line editor component for the TUI part of liberty
  *
- * Copyright (c) 2017 - 2018, Přemysl Janouch <p@janouch.name>
+ * Copyright (c) 2017 - 2018, Přemysl Eric Janouch <p@janouch.name>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted.
